94edb2f3db84cdca6d347305b8e5a4f4e8c3f604
Mbithe <mbithemwangi@gmail.com>
2024-07-22 14:54:24 -0700

    set up enviroment using prisma ORM express and postgreSQL

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import userRoute from './routers/user.route.js';
+import bookRoute from './routers/booking.route.js';
+import authRoute from './routers/auth.route.js';
+
+config();
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], 
+    credentials: true,
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.use("/api/user", userRoute);
+app.use("/api/orders", bookRoute);
+app.use("/api/admin", authRoute);
+
+
+
+app.listen(3005, (req,res) => {
+    console.log("server is running on port 3005...");
+});
+
